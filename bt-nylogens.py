13b573f27dd76f3e17ede6d379f9957ce693b4be
kpelechrinis <kostas.pelechrinis@gmail.com>
2019-07-28 21:26:44 -0400

    Add files via upload

---
+++ bt-nylogens.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import choix
+
+f = open("results.txt","r")
+
+pairs = dict()
+
+for line in f:
+	if "::" in line:
+		linef = line.rstrip().rsplit("::")
+		pairs[linef[0]] = {'id': linef[0], 'left': int(linef[1].rstrip().rsplit("-")[0].rsplit(".")[0])-1, 'right': int(linef[1].rstrip().rsplit("-")[1].rsplit(".")[0])-1}
+	else:
+		pairs[line.rstrip()[:-2]]['choice'] = line.rstrip()[-1] 
+
+data = []
+
+for k in pairs.keys():
+	if 'choice' in pairs[k]:
+		if pairs[k]['choice'] == 'A':
+			el = (pairs[k]['left'],pairs[k]['right'])
+		else:
+			el = (pairs[k]['right'],pairs[k]['left'])
+		data.append(el)
+
+print(len(data))
+skills = choix.ilsr_pairwise(8, data, alpha=0.01)
+
+print(skills)
