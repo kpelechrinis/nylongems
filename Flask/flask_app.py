8b57362ebfdec38be03912ccb542e1c06191b334
kpelechrinis <kostas.pelechrinis@gmail.com>
2019-07-28 21:39:55 -0400

    Update flask_app.py

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -1,238 +1,51 @@
-<!DOCTYPE html>
+from flask import Flask, render_template, Response, request, redirect, url_for
+import os
+import base64
+import fnmatch
+import random
+import datetime
+import sys
 
-<html lang="en" style="height: 100%">
-    <head>
-        <meta charset="utf-8">
-        <title>Login: PythonAnywhere</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Login: PythonAnywhere">
-        <meta name="author" content="PythonAnywhere LLP">
-        <meta name="google-site-verification" content="O4UxDrfcHjC44jybs2vajc1GgRkTKCTRgVzeV6I9V14" />
+TEMPLATE_DIR = os.path.abspath('/home/nylongems/mysite/templates')
+STATIC_DIR = os.path.abspath('/home/nylongems/mysite/static')
 
-
-        <!-- Le styles -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" />
-
-        <link rel="stylesheet" href="/static/CACHE/css/f438f1304d1c.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="/static/CACHE/css/a5c0829f431b.css" type="text/css" /><link rel="stylesheet" href="/static/CACHE/css/2c191bb09ae8.css" type="text/css" media="screen" />
-
-        <!-- Le javascript -->
-        <script type="text/javascript">
-            var Anywhere = {};
-            Anywhere.urls = {};
-            Anywhere.csrfToken = "dfgUSLj3mMItVFmCkaQkNFeQpkw1u0XbGD2cUjmYKxoLl4Z0n4PvToACfZOBVm2F";
-        </script>
-        <script type="text/javascript" src="/static/CACHE/js/fa705b4d7fe1.js"></script>
-        
-
-        <script type="text/javascript" src="/static/CACHE/js/9c3946e0f628.js"></script>
-        
-        
-
-    </head>
-
-     <body style="height:100%;">
-       <div style="min-height: 100%; position: relative;">
-        
-  
-    <nav class="navbar alert alert-warning alert-dismissible cookie-warning" style="padding: 10px 35px 5px 35px; min-height: auto;" id="id_cookie_warning_marker_for_response_middleware">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <p>
-        <small>
-          We use cookies to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media and analytics partners. <a href="/privacy/#cookies">Details here</a>.
-        </small>
-      </p>
-    </nav>
-  
-
-        
-  <nav class="navbar top-nav hidden-xs">
-    <div class="container">
-      <ul class="nav navbar-nav navbar-right">
-        <li class=""><a href="" class="feedback_link">Send feedback</a></li>
-<li class=""><a href="/forums/" class="forums_link">Forums</a></li>
-<li class=""><a href="https://help.pythonanywhere.com/" class="help_link">Help</a></li>
-<li class=""><a href="https://blog.pythonanywhere.com/" class="blog_link">Blog</a></li>
-
-  <li class=""><a style="font-weight: bold;" href="/pricing/" class="pricing_link">Pricing & signup</a></li>
-  <li class=""><a href="/login/?next=/login/" class="login_link">Log in</a></li>
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 
-      </ul>
-    </div>
-  </nav>
+@app.route("/",methods=['GET', 'POST'])
+def index():
+    now = datetime.datetime.now()
+    fout = now.strftime("%Y-%m-%d!%H:%M")+"!"+str(random.randint(0, sys.maxsize))
+    images = [f for f in fnmatch.filter(os.listdir('/home/nylongems/mysite/static/img/'), '*.png')]
+    comp = random.sample(images,2)
+    im1 = open("/home/nylongems/mysite/static/img/"+str(comp[0]),"rb")
+    im1_str = base64.b64encode(im1.read()).decode('ascii')
+    im2 = open("/home/nylongems/mysite/static/img/"+str(comp[1]),"rb")
+    im2_str = base64.b64encode(im2.read()).decode('ascii')
+    competitors = fout+"::"+str(comp[0])+"-"+str(comp[1])
+    fresults = open("/home/nylongems/mysite/results.txt","a")
+    fresults.write(competitors+"\n")
+    fresults.close()
+    return render_template('index.html', image1 = im1_str, image2 = im2_str, v1=fout+"!A",v2=fout+"!B")
 
-  
+@app.route("/forwardA/", methods=['POST'])
+def forwardA():
+    fresults = open("/home/nylongems/mysite/results.txt","a")
+    fresults.write(str(request.form['net'])+"\n")
+    fresults.close()
+    now = datetime.datetime.now()
+    fout = now.strftime("%Y-%m-%d!%H:%M")+"!"+str(random.randint(0, sys.maxsize))
+    images = [f for f in fnmatch.filter(os.listdir('/home/nylongems/mysite/static/img/'), '*.png')]
+    comp = random.sample(images,2)
+    im1 = open("/home/nylongems/mysite/static/img/"+str(comp[0]),"rb")
+    im1_str = base64.b64encode(im1.read()).decode('ascii')
+    im2 = open("/home/nylongems/mysite/static/img/"+str(comp[1]),"rb")
+    im2_str = base64.b64encode(im2.read()).decode('ascii')
+    competitors = fout+"::"+str(comp[0])+"-"+str(comp[1])
+    fresults = open("/home/nylongems/mysite/results.txt","a")
+    fresults.write(competitors+"\n")
+    fresults.close()
+    return render_template('index.html', image1 = im1_str, image2 = im2_str,v1=fout+"!A",v2=fout+"!B");
 
-
-
-
-  <nav class="navbar primary-navbar">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main_nav" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">
-          <img id='id_logo' src="/static/anywhere/images/PA-logo.svg" width="225">
-        </a>
-      </div>
-
-      <div class="collapse navbar-collapse" id="main_nav">
-        <ul class="nav navbar-nav navbar-right">
-          
-            
-
-          
-          <li class="visible-xs"><a href="" class="feedback_link">Send feedback</a></li>
-<li class="visible-xs"><a href="/forums/" class="forums_link">Forums</a></li>
-<li class="visible-xs"><a href="https://help.pythonanywhere.com/" class="help_link">Help</a></li>
-<li class="visible-xs"><a href="https://blog.pythonanywhere.com/" class="blog_link">Blog</a></li>
-
-  <li class="visible-xs"><a style="font-weight: bold;" href="/pricing/" class="pricing_link">Pricing & signup</a></li>
-  <li class="visible-xs"><a href="/login/?next=/login/" class="login_link">Log in</a></li>
-
-
-        </ul>
-      </div>
-
-    </div>
-  </nav>
-
-  <div class="container">
-    
-
-  </div>
-
-  
-
-    
-  
-
-
-        
-  
-
-
-        
-    
-  <div class="container">
-    <div class="row">
-      <div class="col-md-5 col-md-offset-3">
-        
-  <h1>Log in</h1>
-
-  
-
-  <form class="form" action="" method="post"><input type='hidden' name='csrfmiddlewaretoken' value='dfgUSLj3mMItVFmCkaQkNFeQpkw1u0XbGD2cUjmYKxoLl4Z0n4PvToACfZOBVm2F' />
-    <table>
-  <style type="text/css" scoped>
-    /* hide stupid updowney arrows on number input */
-    input[type="number"]::-webkit-outer-spin-button,
-    input[type="number"]::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-    input[type="number"] {
-      -moz-appearance: textfield;
-    }
-  </style>
-  <input type="hidden" name="login_view-current_step" value="auth" id="id_login_view-current_step" />
-  
-
-<p class="form-group">
-  <input type="text" name="auth-username" maxlength="100" id="id_auth-username" autocapitalize="off" autocorrect="off" required placeholder="Username or email address" class="form-control" tabindex="1" />
-  <span class="help-block">
-    No account? <a href="/pricing/" id="id_signup_link">Sign up here!</a>
-  </span>
-</p>
-<p class="form-group">
-  <input type="password" name="auth-password" id="id_auth-password" required placeholder="Password" class="form-control" tabindex="2" />
-  <span class="help-block">
-    <a href="/password_reset/">Forgotten password?</a>
-  </span>
-</p>
-<div class="clear"></div>
-
-</table>
-
-
-    
-    <div style="margin-left: -9999px"><input type="submit" value=""/></div>
-
-    
-
-    
-    <button id="id_next" type="submit" class="btn btn-primary">Log in</button>
-
-    
-
-  </form>
-
-      </div>
-    </div>
-  </div>
-
-
-
-        
-  <div class="footer-spacer"></div>
-  <footer id="id_copyright_div" class="footer">
-    <p>
-      Copyright &copy; 2011-2019 <a href="/about/company_details/">PythonAnywhere LLP</a>
-      &mdash;
-      <a href="/terms/">Terms</a>
-      &mdash;
-      <a href="/privacy/">Privacy & Cookies</a><br/>
-      </p>
-  </footer>
-
-      </div>
-
-        
-
-
-        <div id="id_feedback_dialog" title="Help us improve" style="display:none">
-    <div id="id_feedback_dialog_blurb_big" class="dialog_blurb_big">
-        It's always a pleasure to hear from you!
-    </div>
-    <div id="id_feedback_dialog_blurb_small">
-        Ask us a question, or tell us what you love or hate about PythonAnywhere.<br/>
-        We'll get back to you over email ASAP.
-    </div>
-    <textarea id="id_feedback_dialog_text" rows="6"></textarea>
-    <input id="id_feedback_dialog_email_address" type="text" placeholder="Email address (optional - only necessary if you would like us to contact you)"/>
-    
-    <div id="id_feedback_dialog_error" class="pa_hidden">
-        Sorry, there was an error connecting to the server. <br/>Please try again in a few moments...
-    </div>
-    <div class="dialog_buttons">
-        <img id="id_feedback_dialog_spinner" src="/static/anywhere/images/spinner-small.gif" />
-        <button class="btn btn-primary" id="id_feedback_dialog_ok_button">OK</button>
-        <button class="btn btn-default" id="id_feedback_dialog_cancel_button">Cancel</button>
-    </div>
-</div>
-
-
-        
-            <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-                ga('create', 'UA-18014859-6', 'auto');
-                ga('send', 'pageview');
-            </script>
-        
-
-        
-        <!-- preload font awesome fonts to avoid glitch when switching icons -->
-        <div style="width: 0; height: 0; overflow: hidden"><i class="fa fa-square-o fa-3x" ></i></div>
-    </body>
-</html>
+if __name__ == '__main__':
+	app.run(threaded=True,debug = True)
